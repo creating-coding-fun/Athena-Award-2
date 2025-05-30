1ec88781034bb0190f714375e7687823123b2d8d
Space_Duck <marcnavya@gmail.com>
2025-05-30 15:26:56 -0400

    Create Flashing_shape_game.py (Initial Commit)
    Started my game

---
+++ Flashing_shape_game.py
@@ -0,0 +1,15 @@
+import random
+import turtle
+import time
+
+screen = turtle.Screen()
+screen.setup(700, 700)
+screen.bgcolor("black")
+screen.title("Memory Challenge Game")
+screen.tracer(0)
+
+pen = turtle.Turtle()
+pen.hideturtle()
+pen.speed()
+pen.color("black")
+
