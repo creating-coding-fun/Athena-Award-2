a13f1d90c844d5e8dfdc541ba6e3eccd9c560af3
Space_Duck <marcnavya@gmail.com>
2025-06-01 18:30:36 -0400

    Hour 1 Commit Flashing_shape_game.py
    What I added:
    -The functions to draw the shapes, flash the shape, show the shape before flashing, and flashing a random sequence
    -with lists for shapes and a list for sequence, etc

--- Flashing_shape_game.py
+++ Flashing_shape_game.py
@@ -1,6 +1,6 @@
-import random
 import turtle
 import time
+import random
 
 screen = turtle.Screen()
 screen.setup(700, 700)
@@ -11,5 +11,66 @@ screen.tracer(0)
 pen = turtle.Turtle()
 pen.hideturtle()
 pen.speed()
-pen.color("black")
+
+shapes = ["circe", "square", "triangle"]
+sequence = []
+user_sequence = []
+
+shapes_positions = {"circle: (-100, 0), square: (0, 0), triangel: (100, 0)"}
+
+def drawing_shape (shape, x,y, color="white"):
+    pen.penup()
+    pen.goto(x, y)
+    pen.pendown()
+    pen.color(color)
+    pen.begin_fill()
+    if shape == "circle":
+        pen.circle(60)
+    elif shape == "square":
+        for i in range(4):
+            pen.forward(100)
+            pen.right(90)
+    elif shape == "triangle":
+        for i in range(3):
+            pen.forward(100)
+            pen.left(120)
+    pen.end_fill()
+    screen.update()
+
+    def flash_shape(shape):
+        x, y = shapes_positions [shape]
+        drawing_shape(shape, x, y, "blue")
+        time.sleep(0.5)
+        drawing_shape(shape, x, y, "green")
+        time.sleep(0.5)
+    
+    def show_shapes():
+        pen.clear()
+        for shape in shapes:
+            drawing_shape(shape *shapes_positions[shape])
+            screen.update
+
+    
+    def flash_sequence():
+        screen.tracer(0)
+        show_shapes()
+        screen.tracer(1)
+        time.sleep(1)
+
+        for i in range(5):
+            shape = random.choice(shapes)
+            sequence.append[shape]
+            flash_shape(shape)
+        print("Shape sequence has been shown!! Type it in now (eg. circle, triangle, square, triangle, circle)")
+
+  
+
+
+    
+
+
+
+
+
+
 
