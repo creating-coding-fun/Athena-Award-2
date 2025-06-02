02dce97d00de3c9886141e2b88ceec9764aa2467
Space_Duck <marcnavya@gmail.com>
2025-06-02 12:43:55 -0400

    Hour 4 Commit Flashing_shape_game.py
    What I changed:
    -added a "ready set go" feature in between the shape shape preview and the shape flash sequence
    -fixed the checking user input by specifying how to type in the input to the user
    -attempting to add rounds to the game but its not really working right now

--- Flashing_shape_game.py
+++ Flashing_shape_game.py
@@ -12,6 +12,7 @@ pen = turtle.Turtle()
 pen.hideturtle()
 pen.speed(0)
 
+colors = ["pink", "blue", "yellow", "orange", "purple", "red", "magenta", "green"]
 shapes = ["circle", "square", "triangle"]
 sequence = []
 user_sequence = []
@@ -43,15 +44,36 @@ def drawing_shape (shape, x,y, color="white"):
     screen.update()
 
 def flash_shape(shape):
+    color = random.choice(colors)
     x, y = shapes_positions [shape]
-    drawing_shape(shape, x, y, "blue")
+    drawing_shape(shape, x, y, color)
     time.sleep(0.5)
-    drawing_shape(shape, x, y, "green")
+    drawing_shape(shape, x, y, color)
     time.sleep(0.3)
 
     pen.clear()
     screen.update()
     time.sleep(0.2)
+
+def ready_set_go ():
+    pen.penup()
+    pen.goto(0, 0)
+    pen.color(random.choice(colors))
+    pen.write("Ready?", align = "center", font=("Inconsolata", 50, "bold"))
+    time.sleep(1)
+    pen.clear()
+    pen.penup()
+    pen.goto(0, 0)
+    pen.color(random.choice(colors))
+    pen.write("Set!", align = "center", font=("Inconsolata", 50, "bold"))
+    time.sleep(1)
+    pen.clear()
+    pen.penup()
+    pen.goto(0, 0)
+    pen.color(random.choice(colors))
+    pen.write("Go!!", align = "center", font=("Inconsolata", 50, "bold"))
+    time.sleep(1)
+    pen.clear()
 
 
 def preview_shapes():
@@ -73,16 +95,23 @@ def flash_sequence():
     preview_shapes()
     screen.tracer(1)
     time.sleep(1)
+    ready_set_go()
+    screen.tracer(1)
+    time.sleep(1)
 
     for i in range(5):
         shape = random.choice(shapes)
         sequence.append(shape)
         flash_shape(shape)
-    print("Shape sequence has been shown!! Type it in now (eg. circle, triangle, square, triangle, circle)")
+        time.sleep(0.5)
+        pen.penup()
+        pen.goto(0,0)
+    pen.write("Shape sequence has been shown!! Type it in now:", align = "center", font=("Arial", 16, "normal"))
+    time.sleep(1)
 
 def handling_input():
     global user_input
-    user_input = screen.textinput("Memory Challenge", "Your Answer: Type it in now >:)")
+    user_input = screen.textinput("Memory Challenge", "Your Answer: Type it in now >:), type it as a list without commas. (eg. circle square triangle circle square)")
     check_input()
 
 def check_input():
@@ -91,18 +120,30 @@ def check_input():
     pen.goto(0, -100)
     pen.write(f"Your answer, {typed_input}", align = "center", font=("Arial", 16, "normal"))
     pen.goto(0, -150)  
-    pen.write(f"Correct sequence, {sequence}", align = "center", font=("Arial", 16, "normal"))
+    pen.write(f"Correct sequence, {sequence}", align = "center", font=("Arial", 16, "bold"))
     if typed_input == sequence:
         screen.textinput("Result", "Yaaay! You are correct! :D Press esc to exit!")
     else:
         screen.textinput("Result", "Oop...I guess you couldn't remember :/..press esc to exit.")
     turtle.bye()
 
+def play_rounds():
+    while True:
+        rounds = screen.textinput("Rounds", "How many rounds would you like to play?")
+        if rounds.isdigit():
+            rounds = int(rounds)
+            if rounds <= 0:
+                screen.textinput("Rounds", "Invalid input! Try again!")
+                continue
 
+
+
+
+play_rounds()
 flash_sequence()
 screen.ontimer(handling_input, 500)
 
-screen.mainloop()      
+screen.mainloop()       
 
 
     
